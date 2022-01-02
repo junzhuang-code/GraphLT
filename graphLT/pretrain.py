e0cbded45aa331eadaa3381287320365e531500b
Jun Zhuang <65136293+junzhuang-code@users.noreply.github.com>
2022-01-02 14:51:26 -0500

    Update pretrain.py

--- graphLT/pretrain.py
+++ graphLT/pretrain.py
@@ -3,7 +3,7 @@
 """
 @title: Deperturbation of Online Social Networks via Bayesian Label Transition
 @topic: GCNs' training, prediction, and evaluation
-@authors: Anonymous Authors
+@authors: Jun Zhuang, Mohammad Al Hasan
 """
 
 import os
@@ -113,7 +113,7 @@ def evaluation(model, optimizer, path, graph, feat, label, test_mask): # for dgl
     @return: print out the test acc.
     """
     try:
-        if not len(graph) == len(feat) == len(label) == len(test_mask):
+        if not graph.number_of_nodes() == len(feat) == len(label) == len(test_mask):
             return "The length of adj/feat/label/test_mask is not equal!"
         model.eval()
         model, optimizer, start_epoch, best_acc = load_checkpoint(path, model, optimizer)
@@ -133,7 +133,7 @@ def prediction(model, optimizer, path, graph, feat):
     @return: predicted label (1D Tensor), probabilistic matrix (2D Tensor).
     """
     try:
-        if len(graph) != len(feat):
+        if graph.number_of_nodes() != len(feat):
             return "The length of adj/feat is not equal!"
         model.eval()
         model, optimizer, _, _ = load_checkpoint(path, model, optimizer)
@@ -159,7 +159,7 @@ if __name__ == "__main__":
         data_name = "cora" # kdd20_s1, kdd20_s2, cora, citeseer, amazoncobuy, coauthor, reddit
         model_name = "GCN" # GCN, SGC, GraphSAGE, TAGCN, GIN
         NUM_EPOCHS = 200
-        GPU = -1
+        GPU = 0
         NOISY_RATIO = 0.1
         is_trainable = True
     CUT_RATE = 0.3
